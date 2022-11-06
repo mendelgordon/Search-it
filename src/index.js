5810d57786ad7c9dd96be54f0923d7f0ca91b787
Mendel Gordon <mendelgordons@gmail.com>
2022-11-05 22:27:38 -0400

    Don't import route until it's used

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
 	{
