a25fa2d259c93c94cf67a14909453de900540abd
Mendel Gordon <mendelgordons@gmail.com>
2022-11-05 20:40:29 -0400

    Set app test to smokescreen

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,7 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 import App from "./App";
+import { render } from "@testing-library/react";
 
-test("renders learn react link", () => {
-	render(
-		<Provider store={store}>
-			<App />
-		</Provider>
-	);
-
-	expect(screen.getByText(/learn/i)).toBeInTheDocument();
+test("it renders without crashing", () => {
+	const div = document.createElement("div");
+	render(<App />, div);
 });
