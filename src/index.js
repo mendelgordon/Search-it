e2c856bd086e37c0af46292066979c7f29005f31
Mendel Gordon <mendelgordons@gmail.com>
2022-11-06 22:18:34 -0500

    Remove unused router and css

--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-]);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<App />
 	</React.StrictMode>
 );
 
