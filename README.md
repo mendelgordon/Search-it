4d17a59319917cf181efb042558dc25a2f1835e6
Mendel Gordon <mendelgordons@gmail.com>
2022-11-30 23:26:35 -0500

    remove displaypost inner function and add todo

--- README.md
+++ README.md
@@ -4,5 +4,11 @@ So far this lets you search reddit posts and subreddits, and view posts and subr
 
 Original Desktop Wireframe: [https://wireframe.cc/MnwPlP](https://wireframe.cc/MnwPlP) (didn't end up using this)
 
-Next I plan on adding threading to the comments so you can see what's being replied to.
-I also want to add a loading spinner.
+Next I plan on:
+ adding threading to the comments so you can see what's being replied to.
+ adding a loading spinner.
+ making the homepage of the app show r/popular posts.
+ displaying the image of the post on the post page.
+ adding videos to the searchposts and displaypost pages.
+ adding marked-react to the comments so you can see markdown.
+ managing some of the state with rtk.
--- src/DisplayPost.js
+++ src/DisplayPost.js
@@ -1,7 +1,38 @@
-import "./SearchPosts.css";
+import "./DisplayPost.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+export function DisplayPost() {
+	const { subreddit, id } = useParams();
+	const [post, setPost] = useState(null);
+
+	useEffect(() => {
+		fetch(`https://www.reddit.com/r/${subreddit}/comments/${id}.json?raw_json=1`)
+			.then((response) => response.json())
+			.then((json) => json[1].data.children)
+			.then((children) => setPost(children));
+	}, [subreddit, id]);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<i>r/{subreddit}</i>
+			</header>
+			<main>
+				{post && post.map((i, index) => {
+					return (
+						<div key={index}>
+							<p style={{ marginRight: i.data.depth + 'em'}}>{i.data.body}</p>
+						</div>
+					);
+				})}
+			</main>
+		</div>
+	);
+}
+
+
+/*
 export function DisplayPost() {
 	const { subreddit, id } = useParams();
 	const [post, setPost] = useState([]);
@@ -42,3 +73,4 @@ export function DisplayPost() {
 		</div>
 	);
 }
+*/
