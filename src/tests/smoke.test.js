9e7d21fcfcb69946ed77569b43ea11994e31bc8c
Mendel Gordon <mendelgordons@gmail.com>
2023-08-16 22:36:02 -0400

    Update tests (remove missing feature)

--- src/tests/smoke.test.js
+++ src/tests/smoke.test.js
@@ -1,5 +1,4 @@
 import { DisplayPost } from "../DisplayPost";
-import { DisplaySubreddit } from "../DisplaySubreddit";
 import { SearchPosts } from "../SearchPosts";
 import { SearchSubreddits } from "../SearchSubreddits";
 import { render } from "@testing-library/react";
@@ -8,7 +7,6 @@ import { MemoryRouter, Route, Routes } from "react-router-dom";
 // it renders App without crashing
 it("renders all components without crashing", () => {
 	render(<DisplayPost />);
-	render(<DisplaySubreddit />);
 	render(
 		<MemoryRouter>
 			<Routes>
