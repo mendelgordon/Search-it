2b238471b58f33a7cecf8c99f58ad53f3678aac6
Mendel Gordon <mendelgordons@gmail.com>
2023-08-18 03:37:24 -0400

    Add dark mode

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { Outlet, Link } from "react-router-dom";
 import { useState } from "react";
 
 export function App() {
-	const [isLoading, setLoading] = useState(true);
+	const [, setLoading] = useState(true);
+	const [color, setColor] = useState(localStorage.getItem("color") || "dark-mode");
 
 	const loaderElement = document.querySelector(".loader-container");
 	if (loaderElement) {
 		loaderElement.remove();
-		setLoading(!isLoading);
+		setLoading(false);
 	}
 
+	const darkModeToggle = () => {
+		const newMode = color === "dark-mode" ? "light-mode" : "dark-mode";
+		setColor(newMode);
+		localStorage.setItem("color", newMode);
+	};
+
 	return (
-		<div className="App">
+		<div className={color + " App"}>
 			<nav>
 				<ul>
 					<li>
@@ -22,6 +29,9 @@ export function App() {
 						<Link to={"/searchsubreddits"}>Search Subreddits</Link>
 					</li>
 				</ul>
+				<button onClick={darkModeToggle} type="button" aria-label="Toggle dark mode" title="Toggle dark mode">
+					{color === "dark-mode" ? "🌞" : "🌚"}
+				</button>
 			</nav>
 			<Outlet />
 		</div>
